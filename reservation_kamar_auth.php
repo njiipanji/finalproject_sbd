62a0b5017c7c24e51ac84f6ca2cf2e22c808a3ba
njiipanji <njie_23@yahoo.co.id>
2015-12-27 01:13:34 +0700

    connect ke db, insert kamar

--- reservation_kamar_auth.php
+++ reservation_kamar_auth.php
@@ -1,19 +1,49 @@
 <?php
+    session_start();
 	include "connect.php";
-	$id_tamu = $_POST['id_tamu'];
-	$nama_tamu = $_POST['nama_tamu'];
-	$ttl_tamu = $_POST['ttl_tamu'];
-	$alamat_tamu = $_POST['alamat_tamu'];
-	$telp_tamu = $_POST['telp_tamu'];
+	
+    // tangkap form
+    $id_wisma = $_POST['id_wisma'];
+    $id_jkamar = $_POST['id_jenis'];
+    $no_kamar = $_POST['no_kamar'];
 
-	$new_tamu = "insert into TAMU values ('".$id_tamu."','".$nama_tamu."',TIMESTAMP'".$ttl_tamu." 00:00:00','".$alamat_tamu."','".$telp_tamu."')";
-	$new_tamu_exec = $conn->exec($new_tamu);
-	if ($new_tamu_exec) 
-    {
-        header("Location: http://localhost/finalproject_sbd/reservation_final.php");
+    // tabel transaksi
+    $id_transaksi = $_POST['id_transaksi'];
+    $_SESSION['idtransaksi'] = $id_transaksi;
+    $id_petugas = $_SESSION['userlogin'];
+//  $id_tamu = "";
+//  $tgl_transaksi = "";
+    $tgl_checkin = $_POST['check_in'];
+    $tgl_checkout = $_POST['check_out'];
+    $selisih_tgl = strtotime($tgl_checkout) - strtotime($tgl_checkin);
+    $lama_inap = floor($selisih_tgl/86400);
+//  $tgl_bayar =
+//  $denda = 
+    $query = "select harga_jenis_kamar
+              from jenis_kamar
+              where id_jenis_kamar='".$id_jkamar."'";
+    $harga = $conn->query($query)->fetchAll();
+
+    foreach ((array)$harga as $total) {
+        $total_bayar = $total[0];
     }
-    else
-    {
-        header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php?status=gagal");
+
+	$new_transaksi = "insert into TRANSAKSI_SEWA_KAMAR values('".$id_transaksi."','".$id_petugas."','',SYSTIMESTAMP,TIMESTAMP'".$tgl_checkin." 10:00:00',TIMESTAMP'".$tgl_checkout." 10:00:00','".$lama_inap."',SYSTIMESTAMP,'0','".$total_bayar."')";
+    $new_transaksi_exec = $conn->exec($new_transaksi);
+   
+	if ($new_transaksi_exec) {
+        $new_menyewa = "insert into MENYEWA values('".$no_kamar."','".$id_transaksi."')";
+        $new_menyewa_exec = $conn->exec($new_menyewa);
+        if ($new_menyewa_exec) {
+            $update_room = "update KAMAR set STATUS_KAMAR='1' where NO_KAMAR='".$no_kamar."'";
+            $conn->exec($update_room);
+            header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php");
+        }
+        else {
+            header("Location: http://localhost/finalproject_sbd/reservation.php?status=gagal");
+        }
+    }
+    else {
+        header("Location: http://localhost/finalproject_sbd/reservation.php?status=gagal");
     }
 ?>
