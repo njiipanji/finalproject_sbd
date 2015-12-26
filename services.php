c3270c4902e233a445a58a5e8128b4f9ec1fc979
njiipanji <njie_23@yahoo.co.id>
2015-12-26 12:39:16 +0700

    edit title

--- services.php
+++ services.php
@@ -10,7 +10,7 @@
 		<!--Let browser know website is optimized for mobile-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<title>Guest House ITS - Information System</title>
+		<title>Guest House ITS - Services</title>
 	</head>
 	<body>
 		<header>
