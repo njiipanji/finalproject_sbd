2e925185613739b47078cd424ff382f0070616ca
njiipanji <njie_23@yahoo.co.id>
2016-01-03 22:35:48 +0700

    add pilihan tanggal checkin,checkout; bulan transaksi

--- transactions.php
+++ transactions.php
@@ -62,8 +62,26 @@
 													<i class="medium material-icons">hotel</i>
 												</div>
 												<h5 class="center-align">CEK KAMAR KOSONG</h5>
-												<p class="center-align">Menampilkan kamar-kamar kosong dari berbagai wisma.</p><br><br>
-												<div class="col s12 m12 l12 center" id="cekKamarKosong">
+												<p class="center-align">Menampilkan kamar-kamar kosong dari berbagai wisma.</p><br>
+												<div class="col s12 m11 l11" id="tglCheckIn">
+													<div class="input-field col s2 center">
+														<i class="small material-icons">date_range</i>
+													</div>
+													<label class="col s10" for="check_in">Check-In</label>
+													<div class="col s10">
+														<input name="check_in" id="check_in" type="date" class="datepicker">
+													</div>
+												</div>
+												<div class="col s12 m11 l11" id="tglCheckOut">
+													<div class="input-field col s2 center">
+														<i class="small material-icons">date_range</i>
+													</div>
+													<label class="col s10" for="check_out">Check-Out</label>
+													<div class="col s10">
+														<input name="check_out" id="check_out" type="date" class="datepicker">
+													</div>
+												</div>
+												<div class="col s12 m12 l12 center" id="cekKamarKosong"><br><br>
 													<button class="btn waves-effect waves-light" type="submit">CEK
 														<i class="material-icons right">send</i>
 													</button>
@@ -93,7 +111,7 @@
 													<i class="medium material-icons">account_balance</i>
 												</div>
 												<h5 class="center-align">CEK KEUANGAN</h5>
-												<p class="center-align">Menampilkan pemasukan dan pengeluaran dari berbagai wisma.</p><br><br>
+												<p class="center-align">Menampilkan pemasukan dari berbagai wisma.</p><br><br>
 												<div class="col s12 m12 l12 center" id="cekBalance">
 													<button class="btn waves-effect waves-light" type="submit">CEK
 														<i class="material-icons right">send</i>
@@ -124,8 +142,31 @@
 													<i class="medium material-icons">attach_money</i>
 												</div>
 												<h5 class="center-align">CEK TRANSAKSI BULAN</h5>
-												<p class="center-align">Menampilkan transaksi di bulan tertentu dari berbagai wisma.</p><br><br>
-												<div class="col s12 m12 l12 center" id="cekTransaksiBulan">
+												<p class="center-align">Menampilkan transaksi di bulan tertentu dari berbagai wisma.</p><br>
+												<div class="col s12 m11 l11" id="transaksi_bulan">
+													<div class="input-field col s2 center">
+														<i class="small material-icons">date_range</i>
+													</div>
+													<label class="col s10" for="transaksi_bulan">Bulan Transaksi</label>
+													<div class="col s10">
+														<select class="browser-default" id="transaksi_bulan" name="transaksi_bulan">
+															<option value="" selected>Pilih Bulan</option>
+															<option value="1">Januari</option>
+															<option value="2">Februari</option>
+															<option value="3">Maret</option>
+															<option value="4">April</option>
+															<option value="5">Mei</option>
+															<option value="6">Juni</option>
+															<option value="7">Juli</option>
+															<option value="8">Agustus</option>
+															<option value="9">September</option>
+															<option value="10">Oktober</option>
+															<option value="11">November</option>
+															<option value="12">Desember</option>
+														</select>
+													</div>
+												</div>
+												<div class="col s12 m12 l12 center" id="cekTransaksiBulan"><br><br>
 													<button class="btn waves-effect waves-light" type="submit">CEK
 														<i class="material-icons right">send</i>
 													</button>
