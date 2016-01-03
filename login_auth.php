1f770229abffd83fbec2b6f3cae5f48f65303e3b
njiipanji <njie_23@yahoo.co.id>
2016-01-03 17:07:37 +0700

    add dialog box untuk login gagal

--- login_auth.php
+++ login_auth.php
@@ -12,11 +12,13 @@
 	$telp = $conn->query($query)->fetchAll();
 
 	foreach ((array)$telp as $telpPassword) {
-		if ($telpPassword[0]==$password) {
-			$_SESSION['userlogin']=$identity;
-			header("Location:index-admin.php?status=LOGIN SUCCESS");
-		} else {
-			header("Location:login.php?status=LOGIN FAILED");
-		}
+		$pwd = $telpPassword[0];
+	}
+
+	if ($pwd==$password) {
+		$_SESSION['userlogin']=$identity;
+		header("Location:index-admin.php?status=LOGIN SUCCESS");
+	} else {
+		echo "<script type='text/javascript'>alert('Login Gagal! ID atau Telephone tidak ditemukan.');document.location='login.php'</script>";
 	}
 ?>
