b07c6a7771e5ca4e23da32ea85ebcd31a98c645a
njiipanji <njie_23@yahoo.co.id>
2016-01-03 17:09:10 +0700

    add confirmation table, sebelum dimasukin ke database

--- reservation_final.php
+++ reservation_final.php
@@ -1,10 +1,12 @@
 <?php
 	session_start();
 	include "connect.php";
-	$query = "select *
-			  from transaksi_sewa_kamar
-			  where id_transaksi='".$_SESSION['idtransaksi']."'";
-	$trans = $conn->query($query)->fetchAll();
+	$query = "select w.nama_wisma, jk.nama_jenis_kamar
+			  from kamar k, wisma w, jenis_kamar jk
+			  where k.no_kamar='".$_SESSION['no_kamar']."' and
+			  		k.id_wisma=w.id_wisma and
+			  		k.id_jenis_kamar=jk.id_jenis_kamar";
+	$rooms = $conn->query($query)->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -49,52 +51,36 @@
 					<br>
 					<h1>RESERVATION</h1>
 					<p class="center-align light"><b>CONFIRMATION</b><br>Cek kembali data di bawah.</p>
+					<h5 class="center-align"><br><b>TRANSAKSI<br><b class='blue-text grey lighten-3'><?php echo $_SESSION['id_transaksi']?></b></b></h5>
 				</div>
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col s12 m12 l12">
-						<div id="input" class="section scrollspy">
-							<div class="row">
-								<h5 class="center-align">TRANSAKSI <?php echo $_SESSION['idtransaksi'];?> </h5>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col s12 m12 offset-m2 l12">
+					<h5><br></h5>
+					<h5 class="center-align"><br><b>DETAIL KAMAR<br></b></h5>
+					<div class="col s12 offset-s2 m12 offset-m2 l12">
 						<table class="centered responsive-table highlight">
 							<thead>
 								<tr>
-									<th>ID TRANSAKSI</th>
-									<th>ID PETUGAS</th>
-									<th>ID TAMU</th>
-									<th>TANGGAL TRANSAKSI</th>
+									<th>WISMA</th>
+									<th>TIPE KAMAR</th>
+									<th>NO KAMAR</th>
 									<th>CHECK IN</th>
 									<th>CHECK OUT</th>
 									<th>LAMA INAP</th>
-									<th>TANGGAL BAYAR</th>
-									<th>DENDA</th>
-									<th>TOTAL</th>
 								</tr>
 							</thead>
 							<tbody>
 								<?php
-									foreach ((array)$trans as $tran) {
+									foreach ((array)$rooms as $room) {
 										?>
 										<tr>
-											<td><?php echo $tran['ID_TRANSAKSI']?></td>
-											<td><?php echo $tran['ID_PETUGAS']?></td>
-											<td><?php echo $tran['ID_TAMU']?></td>
-											<td><?php echo $tran['TGL_TRANSAKSI']?></td>
-											<td><?php echo $tran['TGL_CHECKIN']?></td>
-											<td><?php echo $tran['TGL_CHECKOUT']?></td>
-											<td><?php echo $tran['LAMA_INAP']?></td>
-											<td><?php echo $tran['TGL_BAYAR']?></td>
-											<td><?php echo $tran['DENDA']?></td>
-											<td><?php echo $tran['TOTAL']?></td>
+											<td><?php echo $room['NAMA_WISMA']?></td>
+											<td><?php echo $room['NAMA_JENIS_KAMAR']?></td>
+											<td><?php echo $_SESSION['no_kamar']?></td>
+											<td><?php echo $_SESSION['tgl_checkin']?></td>
+											<td><?php echo $_SESSION['tgl_checkout']?></td>
+											<td><?php echo $_SESSION['lama_inap']?></td>
 										</tr>
 										<?php
 									}
@@ -104,11 +90,71 @@
 					</div>	
 				</div>
 			</div>
+
 			<div class="container">
 				<div class="row">
-					<form action="reservation_success.php">
+					<h5><br></h5>
+					<h5 class="center-align"><br><b>DETAIL TAMU<br></b></h5>
+					<div class="col s12 offset-s2 m12 offset-m2 l12">
+						<table class="centered responsive-table highlight">
+							<thead>
+								<tr>
+									<th>NO IDENTITAS</th>
+									<th>NAMA</th>
+									<th>TGL LAHIR</th>
+									<th>ALAMAT</th>
+									<th>TELEPHONE</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><?php echo $_SESSION['id_tamu']?></td>
+									<td><?php echo $_SESSION['nama_tamu']?></td>
+									<td><?php echo $_SESSION['ttl_tamu']?></td>
+									<td><?php echo $_SESSION['alamat_tamu']?></td>
+									<td><?php echo $_SESSION['telp_tamu']?></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>	
+				</div>
+			</div>
+
+
+			<div class="container">
+				<div class="row">
+					<h5><br></h5>
+					<h5 class="center-align"><br><b>DETAIL PEMBAYARAN<br></b></h5>
+					<div class="col s12 offset-s2 m12 offset-m2 l12">
+						<table class="centered responsive-table highlight">
+							<thead>
+								<tr>
+									<th>TGL TRANSAKSI</th>
+									<th>TGL BAYAR</th>
+									<th>DENDA</th>
+									<th>TOTAL BAYAR</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><?php echo $_SESSION['tgl_transaksi']?></td>
+									<td><?php echo $_SESSION['tgl_bayar']?></td>
+									<td><?php echo $_SESSION['denda']?></td>
+									<td><?php echo $_SESSION['total_bayar']?></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>	
+				</div>
+			</div>
+
+
+			<div class="container">
+				<div class="row">
+					<h5><br></h5>
+					<form action="reservation_confirm.php">
 						<div class="col s12 m12 l12 center">
-							<button class="btn waves-effect waves-light" type="submit">FINISH
+							<button class="btn waves-effect waves-light" type="submit">CONFIRM
 								<i class="material-icons right">send</i>
 							</button>
 						</div>
