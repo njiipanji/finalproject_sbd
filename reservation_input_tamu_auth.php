ed0ec45ec59c3565efb30f487098d0c4b7ce07fd
njiipanji <njie_23@yahoo.co.id>
2016-01-03 17:09:36 +0700

    add session dari masing2 post submit

--- reservation_input_tamu_auth.php
+++ reservation_input_tamu_auth.php
@@ -3,25 +3,19 @@
 	include "connect.php";
 	
 	$id_tamu = $_POST['id_tamu'];
-	$nama_tamu = $_POST['nama_tamu'];
-	$ttl_tamu = $_POST['ttl_tamu'];
-	$alamat_tamu = $_POST['alamat_tamu'];
-	$telp_tamu = $_POST['telp_tamu'];
+	$_SESSION['id_tamu'] = $id_tamu;
 
-	$new_tamu = "insert into TAMU values ('".$id_tamu."','".$nama_tamu."',TIMESTAMP'".$ttl_tamu." 00:00:00','".$alamat_tamu."','".$telp_tamu."')";
-	$new_tamu_exec = $conn->exec($new_tamu);
-	if ($new_tamu_exec) {
-		$update_transaksi = "update TRANSAKSI_SEWA_KAMAR set ID_TAMU='".$id_tamu."' where ID_TRANSAKSI='".$_SESSION['idtransaksi']."'";
-		$update_transaksi_exec = $conn->exec($update_transaksi);
-		if ($update_transaksi_exec) {
-			header("Location: http://localhost/finalproject_sbd/reservation_final.php");
-		} else {
-			$delete_tamu = "delete from TAMU where id_tamu='".$id_tamu."'";
-			$conn->exec($delete_tamu);
-			header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php?status=gagal");
-		}
-    }
-    else {
-        header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php?status=gagal");
-    }
+	$nama_tamu = $_POST['nama_tamu'];
+	$_SESSION['nama_tamu'] = $nama_tamu;
+
+	$ttl_tamu = $_POST['ttl_tamu'];
+	$_SESSION['ttl_tamu'] = $ttl_tamu;
+
+	$alamat_tamu = $_POST['alamat_tamu'];
+	$_SESSION['alamat_tamu'] = $alamat_tamu;
+
+	$telp_tamu = $_POST['telp_tamu'];
+	$_SESSION['telp_tamu'] = $telp_tamu;
+
+	header("Location: http://localhost/finalproject_sbd/reservation_final.php");
 ?>
--- reservation_kamar_auth.php
+++ reservation_kamar_auth.php
@@ -2,48 +2,45 @@
     session_start();
 	include "connect.php";
 	
-    // tangkap form
     $id_wisma = $_POST['id_wisma'];
+    $_SESSION['id_wisma'] = $id_wisma;
+    
     $id_jkamar = $_POST['id_jenis'];
+    $_SESSION['id_jenis'] = $id_jkamar;
+    
     $no_kamar = $_POST['no_kamar'];
+    $_SESSION['no_kamar'] = $no_kamar;
 
-    // tabel transaksi
     $id_transaksi = $_POST['id_transaksi'];
-    $_SESSION['idtransaksi'] = $id_transaksi;
-    $id_petugas = $_SESSION['userlogin'];
-//  $id_tamu = "";
-//  $tgl_transaksi = "";
+    $_SESSION['id_transaksi'] = $id_transaksi;
+
+    $tgl_transaksi = date('Y-m-d');
+    $_SESSION['tgl_transaksi'] = $tgl_transaksi;
+
     $tgl_checkin = $_POST['check_in'];
+    $_SESSION['tgl_checkin'] = $tgl_checkin;
+
     $tgl_checkout = $_POST['check_out'];
+    $_SESSION['tgl_checkout'] = $tgl_checkout;
+
     $selisih_tgl = strtotime($tgl_checkout) - strtotime($tgl_checkin);
     $lama_inap = floor($selisih_tgl/86400);
-//  $tgl_bayar =
-//  $denda = 
+    $_SESSION['lama_inap'] = $lama_inap;
+
+    $tgl_bayar = date('Y-m-d');
+    $_SESSION['tgl_bayar'] = $tgl_bayar;
+
+    $denda = 0;
+    $_SESSION['denda'] = $denda;
+
     $query = "select harga_jenis_kamar
               from jenis_kamar
               where id_jenis_kamar='".$id_jkamar."'";
     $harga = $conn->query($query)->fetchAll();
-
     foreach ((array)$harga as $total) {
         $total_bayar = $total[0];
     }
+    $_SESSION['total_bayar'] = $total_bayar;
 
-	$new_transaksi = "insert into TRANSAKSI_SEWA_KAMAR values('".$id_transaksi."','".$id_petugas."','',SYSTIMESTAMP,TIMESTAMP'".$tgl_checkin." 10:00:00',TIMESTAMP'".$tgl_checkout." 10:00:00','".$lama_inap."',SYSTIMESTAMP,'0','".$total_bayar."')";
-    $new_transaksi_exec = $conn->exec($new_transaksi);
-   
-	if ($new_transaksi_exec) {
-        $new_menyewa = "insert into MENYEWA values('".$no_kamar."','".$id_transaksi."')";
-        $new_menyewa_exec = $conn->exec($new_menyewa);
-        if ($new_menyewa_exec) {
-            $update_room = "update KAMAR set STATUS_KAMAR='1' where NO_KAMAR='".$no_kamar."'";
-            $conn->exec($update_room);
-            header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php");
-        }
-        else {
-            header("Location: http://localhost/finalproject_sbd/reservation.php?status=gagal");
-        }
-    }
-    else {
-        header("Location: http://localhost/finalproject_sbd/reservation.php?status=gagal");
-    }
+    header("Location: http://localhost/finalproject_sbd/reservation_input_tamu.php");
 ?>
