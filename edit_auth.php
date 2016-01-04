542b7c5f5f5ab867bc1df094a3d0502564b10d52
njiipanji <njie_23@yahoo.co.id>
2016-01-04 12:36:14 +0700

    merubah edit sesuai id petugas yang dipilih

--- edit_auth.php
+++ edit_auth.php
@@ -4,8 +4,9 @@
 
 	$edit_petugas = "update PETUGAS
 					set nama_petugas='".$_POST['nama_petugas']."', alamat_petugas='".$_POST['alamat_petugas']."', telp_petugas='".$_POST['telp_petugas']."'
-					where id_petugas='".$_SESSION['userlogin']."'";
-	$edit_petugas_exec = $conn->exec($edit_petugas);	
+					where id_petugas='".$_POST['id_petugas']."'";
+	$edit_petugas_exec = $conn->exec($edit_petugas);
+	
 	if ($edit_petugas_exec) {
 		echo "<script type='text/javascript'>alert('Update data berhasil!');document.location='edit_profile.php'</script>";
     }
